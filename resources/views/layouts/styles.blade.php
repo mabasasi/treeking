1b0cc68c5326f1e279271f68617e2084ed8aa07e
mabasasi <aoe317293@yahoo.co.jp>
2018-02-16 00:38:24 +0900

    [add]HTML フレームの作成

---
+++ resources/views/layouts/styles.blade.php
@@ -0,0 +1,9 @@
+
+<link rel="stylesheet" href="//cdn.honokak.osaka/honoka/4.0.0-alpha.1/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.0-alpha14/css/tempusdominus-bootstrap-4.min.css">
+
+
+<style>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
-Route::get('/test', 'TestController')->name('test');
+Route::get('/home', 'TestController')->name('test');
