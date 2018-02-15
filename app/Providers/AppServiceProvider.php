bd80f07f9a681f7f44430f87a5c4e698e6f4a747
mabasasi <aoe317293@yahoo.co.jp>
2018-02-16 04:24:48 +0900

    [add]validator ファイル作成

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Validators\DatabaseValidator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // extend validator
+        \Validator::resolver(function ($translator, $data, $rules, $messages) {
+            return new DatabaseValidator($translator, $data, $rules, $messages);
+        });
     }
 
     /**
