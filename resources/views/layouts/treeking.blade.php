9035959e8df6c50c9d09334cdfc133bf48e2008c
mabasasi <aoe317293@yahoo.co.jp>
2018-02-16 14:17:06 +0900

    [add]request に error = true で validation error を強制表示するビューを作成

--- resources/views/layouts/treeking.blade.php
+++ resources/views/layouts/treeking.blade.php
@@ -4,6 +4,11 @@
     <!-- Navivation Bar -->
     @includeIf('layouts.navbar')
 
+    @if(request('error') == 'true')
+        <!-- Debug Error View-->
+        @includeIf('layouts.error')
+    @endif
+
     <!-- Main Content -->
     @yield('content')
 @endsection
