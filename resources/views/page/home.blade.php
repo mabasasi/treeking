24ea09914752544201273c870bf3384f2b2247cb
mabasasi <aoe317293@yahoo.co.jp>
2018-02-18 04:10:10 +0900

    [mod]container 要素を treeking layout に追い出した

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -1,24 +1,22 @@
 @extends('layouts.treeking')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col">
+    <div class="row">
+        <div class="col">
 
-                <div class="jumbotron margin">
-                    <h1 class="display-4">Treeking</h1>
-                    <p class="lead">A web notepad that supports thinking using tree structure.</p>
-                    <hr class="my-4">
-                    <p>
-                        時系列を扱えるメモ帳です.
-                        単純にでも、複雑にでも使うことができます.
-                    </p>
-                    {{--<p class="lead">--}}
-                        {{--<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>--}}
-                    {{--</p>--}}
-                </div>
-
+            <div class="jumbotron margin">
+                <h1 class="display-4">Treeking</h1>
+                <p class="lead">A web notepad that supports thinking using tree structure.</p>
+                <hr class="my-4">
+                <p>
+                    時系列を扱えるメモ帳です.
+                    単純にでも、複雑にでも使うことができます.
+                </p>
+                {{--<p class="lead">--}}
+                    {{--<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>--}}
+                {{--</p>--}}
             </div>
+
         </div>
     </div>
 @endsection
--- resources/views/treeking/index.blade.php
+++ resources/views/treeking/index.blade.php
@@ -2,13 +2,11 @@
 @section('title', 'めも帳')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col">
+    <div class="row">
+        <div class="col">
 
-                index
+            index
 
-            </div>
         </div>
     </div>
 @endsection
