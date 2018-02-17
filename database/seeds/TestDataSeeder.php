101fbba1bc82c6835831e91ba555521e764e568a
mabasasi <aoe317293@yahoo.co.jp>
2018-02-17 21:17:30 +0900

    [add]plant と grow と brae のテスト

--- database/seeds/TestDataSeeder.php
+++ database/seeds/TestDataSeeder.php
@@ -13,6 +13,7 @@ class TestDataSeeder extends Seeder
      * Run the database seeds.
      *
      * @return void
+     * @throws \Illuminate\Validation\ValidationException
      */
     public function run() {
         // 全部消す！
@@ -24,7 +25,14 @@ class TestDataSeeder extends Seeder
         \DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         // テストケースを作成する
+        LeafType::create(['id' => Consts::FRUIT_TYPE_PLANE, 'name' => '標準']);
 
+        $branch_alpha = Branch::create(['name' => 'ALPHA']);
+
+        $sprig_a = $branch_alpha->growWithBearMethod('A', [
+            'content' => 'AAAAA',
+            'leaf_type_id' => Consts::FRUIT_TYPE_PLANE,
+        ]);
 
 
 
