c13e33433b4bc80e10b5b35537b29f6de6490521
mabasasi <aoe317293@yahoo.co.jp>
2018-02-17 22:49:25 +0900

    [add]Helper の作成

---
+++ app/Helpers.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mabasasi
+ * Date: 2018/02/17
+ * Time: 22:21
+ */
+
+
+
+
+
+
+
+
+// Blade Helpers
+
+if (! function_exists('out_if_true')) {
+
+    /**
+     * bool が true の時に出力するメソッド.
+     * @param bool $bool 比較値
+     * @param string $val true の出力値
+     * @param string $default false の出力値 nullable
+     * @return string 出力文字列
+     */
+    function out_if_true(bool $bool, string $val, string $default = '') {
+        return $bool ? $val : $default;
+    }
+}
