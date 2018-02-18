a8bc114ad425c9484194c7d574a68531c8bc1b91
mabasasi <aoe317293@yahoo.co.jp>
2018-02-18 20:26:36 +0900

    [add]ログイン機能とナビバーを実装

--- database/seeds/UserDataSeeder.php
+++ database/seeds/UserDataSeeder.php
@@ -17,12 +17,13 @@ class UserDataSeeder extends Seeder
         \DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         // 管理用 User の作成
+        $pass = env('ADMIN_PASSWORD', null);
         User::create([
             'id'       => env('id', \App\Consts::ADMIN_USER_ID),
             'name'     => env('ADMIN_NAME',     null),
             'userid'   => env('ADMIN_USER_ID',  null),
             'email'    => env('ADMIN_EMAIL',    null),
-            'password' => env('ADMIN_PASSWORD', null),
+            'password' => $pass ? Hash::make($pass) : null,
         ]);
     }
 }
